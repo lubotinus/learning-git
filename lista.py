e096a70045820235ff4dc36c79f00105348d9f93
Dmitry Barankov <dmitry.barankov@gmail.com>
2023-02-16 19:33:22 +0100

    New git

---
+++ lista.py
@@ -0,0 +1,10 @@
+print("Zadanie 3.1")
+d5=[]
+d3=[]
+for x in range (0, 100):
+    if x%5==0:
+        d5.append(x)
+        x=x**3
+        d3.append(x)
+print(f"Liczby dzielone na 5: {d5}")
+print(f"Liczby dzielone na 5 podniesione do potęgi 3: {d3}")
